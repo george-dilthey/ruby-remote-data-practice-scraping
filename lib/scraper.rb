f18b16c4467384d53fa99ff71a4d5c9fc9fc244e
George Dilthey <george.dilthey@gmail.com>
2021-03-08 22:38:57 -0500

    commit tree 49a68dde36b89120f22772ca788494a121bd128b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+header = doc.css("h1.headline-26OIBN").text
+courses = doc.css(".inlineMobileLeft-2Yo002")
+courses.each do |course|
+    puts courses.css("title").text
+    puts courses.css("copy p").text
+end
+
+
+
+binding.pry
